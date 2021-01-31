52cd5eb74f579b838f585ff3ec4c6e99f8d9f3ee
bsaglam <burcu.saglam001@gmail.com>
2021-01-31 21:01:46 +0300

    sayfanın componentlere ayrılması

---
+++ assignment6-components/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+<div class="container">
+    <div class=row>
+      <Header></Header>
+    </div>
+    <hr>
+    <div class="row">
+      <Body></Body>
+    </div>
+    <hr>
+    <div class="row">
+      <Footer></Footer>
+    </div>
+</div>
+</template>
+
+<script>
+import Header from './Header'
+import Footer from './Footer'
+import Body from './Body'
+export default {
+  components:{'Header':Header,'Footer':Footer,'Body':Body}
+}
+</script>
+
+ 
---
+++ assignment6-components/src/Footer.vue
@@ -0,0 +1,13 @@
+<template>
+<div class="col-xs-12">
+    <footer>
+          <p>Tüm Hakları Saklıdır | videosinif.com</p>
+    </footer>
+</div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
