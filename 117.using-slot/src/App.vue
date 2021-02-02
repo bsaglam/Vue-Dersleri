0a1193079c79f8e18bb15bd84020b9ad53ae4a30
bsaglam <burcu.saglam001@gmail.com>
2021-02-02 06:32:37 +0300

    slot kullanımı

---
+++ 117.using-slot/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+    <Parent>
+      <h1 slot="heading">Burası paragraf</h1>
+      <p slot="content">burası içerik</p>
+    </Parent>
+  </div>
+</template>
+
+<script>
+import Parent from '../src/components/Parent'
+export default {
+  components : { Parent : Parent }
+   }
+</script>
+
+<style scoped>
+ div{
+     height: 600px;
+     width: 500px;
+     border:1px solid black;
+     
+ }
+</style>
---
+++ 117.using-slot/src/components/Parent.vue
@@ -0,0 +1,34 @@
+
+<template>
+<div>
+    <hr>
+    <div>
+       <slot name="heading"></slot>
+    </div>
+    <hr>
+    <div>
+       <slot name="content"></slot>
+    </div>
+</div>
+  
+</template>
+
+
+
+<script>
+ 
+export default {
+
+     
+}
+</script>
+
+<style scoped>
+div{
+     height: 100px;
+     width: 300px;
+     border:1px solid black;
+     margin: 10px 10px;
+     background-color: dimgray;
+ }
+</style>
